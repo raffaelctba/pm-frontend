652f297050a4a4393808c4a5aa6d757b4a48b242
raffael <raffaelctba@hotmail.com>
2026-05-10 23:46:48 -0700

    added deploy-backend-ec2-prd.yml

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: false,
+  apiBaseUrl: 'http://localhost:8081',
+  wsBaseUrl: 'http://localhost:8081',
+  keycloakUrl: 'http://localhost:8083',
+  keycloakRealm: 'myproperty',
+  keycloakClientId: 'myproperty-angular-frontend'
+};
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: true,
+  apiBaseUrl: '__API_BASE_URL__',
+  wsBaseUrl: '__WS_BASE_URL__',
+  keycloakUrl: '__KEYCLOAK_URL__',
+  keycloakRealm: 'myproperty',
+  keycloakClientId: 'myproperty-angular-frontend'
+};
+
+
